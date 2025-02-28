c96e1e60a3c552047b5af4769d6d19ba6b8fba7f
Boris <shevnin.boris2@yandex.ru>
2025-02-28 13:51:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ContentWrapper from "./components/ContentWrapper/ContentWrapper";
+
+function App() {
+  return (
+    <>
+      <ContentWrapper>Hello</ContentWrapper>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ContentWrapper/ContentWrapper.tsx
@@ -0,0 +1,7 @@
+import styles from "./ContentWrapper.module.scss";
+
+const ContentWrapper = () => {
+  return <div className={styles["container"]}>ContentWrapper</div>;
+};
+
+export default ContentWrapper;
