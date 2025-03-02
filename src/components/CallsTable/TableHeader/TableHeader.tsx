b228a5cf628831265d6789f1e89a3593f448a7f8
Boris <shevnin.boris2@yandex.ru>
2025-03-02 11:24:23 +0300

    commit 14

--- src/components/CallsTable/TableHeader/TableHeader.tsx
+++ src/components/CallsTable/TableHeader/TableHeader.tsx
@@ -1,21 +1,28 @@
 import styles from "./TableHeader.module.scss";
 import { CALLS_TABLE_HEADERS } from "@/constants";
 import openDropdownArrow from "@/assets/icons/ui/openDropdownArrow.svg";
+import IconWrapper from "@/components/containers/IconWrapper/IconWrapper";
 
 const TableHeader = () => {
   return (
     <thead className={styles["table-header"]}>
       <tr className={styles["table-header__row"]}>
-        {CALLS_TABLE_HEADERS.map((header) => (
-          <th key={header} scope="col" className={styles["table-header__cell"]}>
+        {CALLS_TABLE_HEADERS.map((headerCell) => (
+          <th
+            key={headerCell}
+            scope="col"
+            className={styles["table-header__cell"]}
+          >
             <span className={styles["table-header__text"]}>
-              {header}
-              {header === "Длительность" && (
-                <img
-                  src={openDropdownArrow}
-                  alt="Dropdown Arrow"
-                  className={styles["table-header__icon"]}
-                />
+              {headerCell}
+              {(headerCell === "Длительность" || headerCell === "Время") && (
+                <IconWrapper width={18} height={21}>
+                  <img
+                    src={openDropdownArrow}
+                    alt="Dropdown Arrow"
+                    className={styles["table-header__icon"]}
+                  />
+                </IconWrapper>
               )}
             </span>
           </th>
