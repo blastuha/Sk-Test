f0fd53d8ff52cc09fe4ba158e83c368321e44116
Boris <shevnin.boris2@yandex.ru>
2025-03-03 17:45:43 +0300

    commit 20

--- src/components/ui/AudioMessage/AudioMessage.tsx
+++ src/components/ui/AudioMessage/AudioMessage.tsx
@@ -15,29 +15,43 @@ const AudioMessage: React.FC<AudioMessageProps> = ({ time, audioSrc }) => {
 
   return (
     <div className={styles["audio-message"]}>
-      <span className={styles["time"]}>{time}</span>
-      <IconWrapper width={24} height={24}>
-        <button className={styles["play-button"]}>
-          <img src={playIcon} alt="Play" />
-        </button>
-      </IconWrapper>
-      <div className={styles["progress-bar"]} />
-      {isDownloading ? (
+      <span className={styles["audio-message__time"]}>{time}</span>
+
+      <div className={styles["audio-message__player"]}>
         <IconWrapper width={24} height={24}>
           <button
-            className={styles["cancel-button"]}
+            className={
+              styles["audio-message__button audio-message__button--play"]
+            }
+          >
+            <img src={playIcon} alt="Play" />
+          </button>
+        </IconWrapper>
+        <div className={styles["audio-message__progress-bar"]} />
+      </div>
+
+      <div className={styles["audio-message__controls"]}>
+        <IconWrapper width={24} height={24}>
+          <button
+            className={
+              styles["audio-message__button audio-message__button--cancel"]
+            }
             onClick={() => setIsDownloading(false)}
           >
             <img src={closeIcon} alt="Cancel" />
           </button>
         </IconWrapper>
-      ) : (
+
         <IconWrapper width={24} height={24}>
-          <button className={styles["download-button"]}>
+          <button
+            className={
+              styles["audio-message__button audio-message__button--download"]
+            }
+          >
             <img src={downloadIcon} alt="Download" />
           </button>
         </IconWrapper>
-      )}
+      </div>
     </div>
   );
 };
